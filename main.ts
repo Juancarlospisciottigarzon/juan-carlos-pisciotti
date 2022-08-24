ede67965e09c040f7e4b81d9171222e0de2c349d
Juancarlospisciottigarzon <111998353+Juancarlospisciottigarzon@users.noreply.github.com>
2022-08-24 10:04:40 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(1000)
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . # # # .
+        . . # . .
+        . . . . .
+        `)
+    basic.pause(1000)
+})
